49ce6d4c43b17cb350ce3aa6cea72951f8d365d6
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-02-06 12:25:29 -0300

    Create a test case over HasSubscriptions trait

---
+++ tests/Models/Concerns/HasSubscriptionsTest.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace LucasDotDev\Soulbscription\Tests\Feature\Models\Concerns;
+
+use LucasDotDev\Soulbscription\Models\{Feature, FeatureConsumption, Plan, Subscription, SubscriptionRenewal};
+use Illuminate\Foundation\Testing\{RefreshDatabase, WithFaker};
+use LucasDotDev\Soulbscription\Tests\Mocks\Models\User;
+use OutOfBoundsException;
+use OverflowException;
+use LucasDotDev\Soulbscription\Tests\TestCase;
+
+class HasSubscriptionsTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function testModelCanSubscribeToAPlan()
+    {
+        $plan = Plan::factory()->createOne();
+
+        $subscriber   = User::factory()->createOne();
+        $subscription = $subscriber->subscribeTo($plan);
+
+        $this->assertDatabaseHas('subscriptions', [
+            'id'            => $subscription->id,
+            'plan_id'       => $plan->id,
+            'subscriber_id' => $subscriber->id,
+            'expires_at'    => $plan->calculateExpiration(),
+        ]);
+    }
+
+    public function testModelCanConsumeAFeature()
+    {
+        $charges     = $this->faker->numberBetween(5, 10);
+        $consumption = $this->faker->numberBetween(1, $charges);
+
+        $plan    = Plan::factory()->createOne();
+        $feature = Feature::factory()->consumable()->createOne();
+        $feature->plans()->attach($plan, [
+            'charges' => $charges,
+        ]);
+
+        $subscriber = User::factory()->createOne();
+        $subscriber->subscribeTo($plan);
+
+        $subscriber->consume($feature->name, $consumption);
+
+        $this->assertDatabaseHas('feature_consumptions', [
+            'consumption'   => $consumption,
+            'feature_id'    => $feature->id,
+            'subscriber_id' => $subscriber->id,
+        ]);
+    }
+
+    public function testModelCanConsumeANotConsumableFeatureIfItIsAvailable()
+    {
+        $plan    = Plan::factory()->createOne();
+        $feature = Feature::factory()->notConsumable()->createOne();
+        $feature->plans()->attach($plan);
+
+        $subscriber = User::factory()->createOne();
+        $subscriber->subscribeTo($plan);
+
+        $subscriber->consume($feature->name);
+
+        $this->assertDatabaseHas('feature_consumptions', [
+            'consumption'   => null,
+            'feature_id'    => $feature->id,
+            'subscriber_id' => $subscriber->id,
+        ]);
+    }
+
+    public function testModelCantConsumeAnUnavailableFeature()
+    {
+        $charges     = $this->faker->numberBetween(5, 10);
+        $consumption = $this->faker->numberBetween(1, $charges);
+
+        $plan    = Plan::factory()->createOne();
+        $feature = Feature::factory()->consumable()->createOne();
+        $feature->plans()->attach($plan, [
+            'charges' => $charges,
+        ]);
+
+        $subscriber = User::factory()->createOne();
+        $subscriber->subscribeTo($plan, now()->subDay());
+
+        $this->expectException(OutOfBoundsException::class);
+        $this->expectExceptionMessage('None of the active plans grants access to this feature.');
+
+        $subscriber->consume($feature->name, $consumption);
+
+        $this->assertDatabaseMissing('feature_consumptions', [
+            'consumption'   => $consumption,
+            'feature_id'    => $feature->id,
+            'subscriber_id' => $subscriber->id,
+        ]);
+    }
+
+    public function testModelCantConsumeAFeatureBeyondItsCharges()
+    {
+        $charges     = $this->faker->numberBetween(5, 10);
+        $consumption = $charges + 1;
+
+        $plan    = Plan::factory()->createOne();
+        $feature = Feature::factory()->consumable()->createOne();
+        $feature->plans()->attach($plan, [
+            'charges' => $charges,
+        ]);
+
+        $subscriber = User::factory()->createOne();
+        $subscriber->subscribeTo($plan);
+
+        $this->expectException(OverflowException::class);
+        $this->expectExceptionMessage('The feature has no enough charges to this consumption.');
+
+        $subscriber->consume($feature->name, $consumption);
+
+        $this->assertDatabaseMissing('feature_consumptions', [
+            'consumption'   => $consumption,
+            'feature_id'    => $feature->id,
+            'subscriber_id' => $subscriber->id,
+        ]);
+    }
+
+    public function testModelCanConsumeSomeAmountOfAConsumableFeature()
+    {
+        $charges     = $this->faker->numberBetween(5, 10);
+        $consumption = $this->faker->numberBetween(1, $charges);
+
+        $plan    = Plan::factory()->createOne();
+        $feature = Feature::factory()->consumable()->createOne();
+        $feature->plans()->attach($plan, [
+            'charges' => $charges,
+        ]);
+
+        $subscriber = User::factory()->createOne();
+        $subscriber->subscribeTo($plan);
+
+        $modelCanUse = $subscriber->canConsume($feature->name, $consumption);
+
+        $this->assertTrue($modelCanUse);
+    }
+
+    public function testModelCantConsumeSomeAmountOfAConsumableFeatureFromAnExpiredSubscription()
+    {
+        $charges     = $this->faker->numberBetween(5, 10);
+        $consumption = $this->faker->numberBetween(1, $charges);
+
+        $plan    = Plan::factory()->createOne();
+        $feature = Feature::factory()->consumable()->createOne();
+        $feature->plans()->attach($plan, [
+            'charges' => $charges,
+        ]);
+
+        $subscriber = User::factory()->createOne();
+        $subscriber->subscribeTo($plan, now()->subDay());
+
+        $modelCanUse = $subscriber->canConsume($feature->name, $consumption);
+
+        $this->assertFalse($modelCanUse);
+    }
+
+    public function testModelCantConsumeSomeAmountOfAConsumableFeature()
+    {
+        $charges     = $this->faker->numberBetween(5, 10);
+        $consumption = $charges + 1;
+
+        $plan    = Plan::factory()->createOne();
+        $feature = Feature::factory()->consumable()->createOne();
+        $feature->plans()->attach($plan, [
+            'charges' => $charges,
+        ]);
+
+        $subscriber = User::factory()->createOne();
+        $subscriber->subscribeTo($plan);
+
+        $modelCanUse = $subscriber->canConsume($feature->name, $consumption);
+
+        $this->assertFalse($modelCanUse);
+    }
+
+    public function testModelCanConsumeSomeAmountOfAConsumableFeatureIfItsConsumptionsAreExpired()
+    {
+        $charges     = $this->faker->numberBetween(5, 10);
+        $consumption = $this->faker->numberBetween(1, $charges);
+
+        $plan    = Plan::factory()->createOne();
+        $feature = Feature::factory()->consumable()->createOne();
+        $feature->plans()->attach($plan, [
+            'charges' => $charges,
+        ]);
+
+        $subscriber = User::factory()->createOne();
+        $subscriber->subscribeTo($plan);
+
+        FeatureConsumption::factory()
+            ->for($feature)
+            ->for($subscriber, 'subscriber')
+            ->createOne([
+                'consumption' => now()->subDay(),
+                'expires_at'  => $this->faker->dateTime(),
+            ]);
+
+        $modelCanUse = $subscriber->canConsume($feature->name, $consumption);
+
+        $this->assertTrue($modelCanUse);
+    }
+
+    public function testModelHasSubscriptionRenewals()
+    {
+        $subscriber   = User::factory()->createOne();
+        $subscription = Subscription::factory()
+            ->for($subscriber, 'subscriber')
+            ->createOne();
+
+        $renewalsCount = $this->faker->randomDigitNotZero();
+        $renewals      = SubscriptionRenewal::factory()
+            ->times($renewalsCount)
+            ->for($subscription)
+            ->createOne();
+
+        $this->assertEqualsCanonicalizing(
+            $renewals->pluck('id'),
+            $subscriber->renewals->pluck('id'),
+        );
+    }
+}
