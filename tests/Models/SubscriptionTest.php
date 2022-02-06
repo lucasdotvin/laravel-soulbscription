684204b9b74dfe2b770e63531c38171e22cda366
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-02-06 12:25:12 -0300

    Create a test to cover Subscription model

---
+++ tests/Models/SubscriptionTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace LucasDotDev\Soulbscription\Tests\Feature\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use LucasDotDev\Soulbscription\Models\Plan;
+use LucasDotDev\Soulbscription\Models\Subscription;
+use LucasDotDev\Soulbscription\Tests\Mocks\Models\User;
+use LucasDotDev\Soulbscription\Tests\TestCase;
+
+class SubscriptionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testModelRenews()
+    {
+        $plan         = Plan::factory()->create();
+        $subscriber   = User::factory()->create();
+        $subscription = Subscription::factory()
+            ->for($plan)
+            ->for($subscriber, 'subscriber')
+            ->create();
+
+        $subscription->renew();
+
+        $this->assertDatabaseHas('subscriptions', [
+            'plan_id'         => $plan->id,
+            'subscriber_id'   => $subscriber->id,
+            'subscriber_type' => User::class,
+            'expires_at'      => $plan->calculateExpiration(),
+        ]);
+    }
+
+    public function testModelRegistersRenewal()
+    {
+        $subscriber   = User::factory()->create();
+        $subscription = Subscription::factory()
+            ->for($subscriber, 'subscriber')
+            ->create();
+
+        $subscription->renew();
+
+        $this->assertDatabaseCount('subscription_renewals', 1);
+        $this->assertDatabaseHas('subscription_renewals', [
+            'subscription_id' => $subscription->id,
+            'renewal'         => true,
+        ]);
+    }
+
+    public function testModelRegistersOverdue()
+    {
+        $subscriber   = User::factory()->create();
+        $subscription = Subscription::factory()
+            ->for($subscriber, 'subscriber')
+            ->create([
+                'expires_at' => now()->subDay(),
+            ]);
+
+        $subscription->renew();
+
+        $this->assertDatabaseCount('subscription_renewals', 1);
+        $this->assertDatabaseHas('subscription_renewals', [
+            'subscription_id' => $subscription->id,
+            'overdue'         => true,
+        ]);
+    }
+}
