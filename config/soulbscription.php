73550e95f4eb517304c80743994f9fdb5870efe0
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-02-19 17:20:24 -0300

    Create a config file

--- config/soulbscription.php
+++ config/soulbscription.php
@@ -1,5 +1,18 @@
 <?php
 
 return [
-    'posts_table' => 'posts',
+    'models' => [
+
+        'feature' => \LucasDotDev\Soulbscription\Models\Feature::class,
+
+        'feature_consumption' => \LucasDotDev\Soulbscription\Models\FeatureConsumption::class,
+
+        'feature_plan' => \LucasDotDev\Soulbscription\Models\FeaturePlan::class,
+
+        'plan' => \LucasDotDev\Soulbscription\Models\Plan::class,
+
+        'subscription' => \LucasDotDev\Soulbscription\Models\Subscription::class,
+
+        'subscription_renewal' => \LucasDotDev\Soulbscription\Models\SubscriptionRenewal::class,
+    ],
 ];
