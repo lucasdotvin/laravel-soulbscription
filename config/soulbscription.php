8f832bf2b0ca00e15bd3412847316bbeb5c6f6ea
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-02-19 17:06:57 -0300

    Register migrations as a publishable resource

---
+++ config/soulbscription.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'posts_table' => 'posts',
+];
--- src/SoulbscriptionServiceProvider.php
+++ src/SoulbscriptionServiceProvider.php
@@ -9,5 +9,14 @@ class SoulbscriptionServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->mergeConfigFrom(__DIR__ . '/../config/soulbscription.php', 'soulbscription');
+
+        $this->publishes([
+            __DIR__ . '/../config/soulbscription.php' => config_path('soulbscription.php'),
+        ], 'soulbscription-config');
+
+        $this->publishes([
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
+        ], 'soulbscription-migrations');
     }
 }
