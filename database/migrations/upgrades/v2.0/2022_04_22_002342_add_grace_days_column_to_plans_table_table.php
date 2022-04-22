18cda6217b3e2841af925e0ceb7cd5ad0eb9a6c3
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-04-22 18:18:08 -0300

    Create a migration group to upgrade the database structure

---
+++ database/migrations/upgrades/v2.0/2022_04_22_002342_add_grace_days_column_to_plans_table_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('plans', function (Blueprint $table) {
+            $table->integer('grace_days')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('plans', function (Blueprint $table) {
+            $table->dropColumn('grace_days');
+        });
+    }
+};
---
+++ database/migrations/upgrades/v2.0/2022_04_22_002342_add_grace_ended_at_column_to_subscriptions_table_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->timestamp('grace_days_ended_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->dropColumn('grace_days_ended_at');
+        });
+    }
+};
