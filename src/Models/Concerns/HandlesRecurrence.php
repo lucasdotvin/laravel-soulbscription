3022c25bb9cf935015ab7798f25fcae4c97e70cc
Lucas Vinicius <lucasvinicius211@yahoo.com>
2024-04-08 00:49:14 -0300

    fix: Fix date difference parameters order

--- src/Models/Concerns/HandlesRecurrence.php
+++ src/Models/Concerns/HandlesRecurrence.php
@@ -17,7 +17,7 @@ trait HandlesRecurrence
             $start = Carbon::parse($start);
         }
 
-        $recurrences = PeriodicityType::getDateDifference(from: now(), to: $start, unit: $this->periodicity_type);
+        $recurrences = PeriodicityType::getDateDifference(from: $start, to: now(), unit: $this->periodicity_type);
         $expirationDate = $start->copy()->add($this->periodicity_type, $this->periodicity + $recurrences);
 
         return $expirationDate;
